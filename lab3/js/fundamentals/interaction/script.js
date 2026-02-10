0b2679b0e4635efb5a82db0edf6e7b3f902a5ee7
Symbat <s_komekbay@kbtu.kz>
2026-02-10 22:54:22 +0500

    a

---
+++ lab3/js/fundamentals/interaction/script.js
@@ -0,0 +1,9 @@
+//comfirm
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
+
+//promt
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!

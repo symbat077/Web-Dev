093c6e08c32e30578d086ea1dbd5aa403d12b092
Symbat <s_komekbay@kbtu.kz>
2026-02-10 23:04:08 +0500

    a

---
+++ lab3/js/fundamentals/variables/var.js
@@ -0,0 +1,19 @@
+let hello = 'Hello world!';
+
+let message;
+
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+alert(hello); // Hello world!
+alert(message); // Hello world!
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00

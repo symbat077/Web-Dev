5da74bcbe99294f07915b03b68c89cf9fabd2529
Symbat <s_komekbay@kbtu.kz>
2026-02-10 23:05:39 +0500

    a

---
+++ lab3/js/fundamentals/data.types/d.js
@@ -0,0 +1,27 @@
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ? hello 1
+
+alert( `hello ${"name"}` ); // ? hello name
+
+alert( `hello ${name}` ); // ? hello Ilya
+
+
+// Infinity
+alert( Infinity ); 
+
+alert( "not a number" / 2 ); // NaN, such division is erroneous     
+
+let age = null; //null
+
+//BOOl
+
+let isGreater = 4 > 1;
+
+alert( isGreater ); // true (the comparison result is "yes")
+
+
+//undefined
+let agee;
+
+alert(age); // shows "undefined"
